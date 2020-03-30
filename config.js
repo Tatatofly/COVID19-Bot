31af5c91f23ce022dcd68768af738c4f70b24c70
Tatu <tatatofly@gmail.com>
2020-03-30 17:10:43 +0300

    Updated config.js

--- config.js
+++ config.js
@@ -1,6 +1,14 @@
 const config = {}
 
-config.discordToken = 'DISCORD_BOT_TOKEN'
-config.prefix = 'c!'
+config.discordToken = 'YOUR_DISCORD_TOKEN'
+config.prefix = '!'
+config.apiCooldown = 10 // In minutes
+
+config.dailyNotifyFile = 'dailyNotify.json'
+config.dailyNotifiedFile = 'dailyNotified.json'
+
+config.apiURL = 'https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData'
+
+config.healthCareDistricts = ['HUS','Lappi','Länsi-Pohja','Pohjois-Pohjanmaa','Kainuu','Keski-Pohjanmaa','Pohjois-Savo','Pohjois-Karjala','Vaasa','Keski-Suomi','Etelä-Pohjanmaa','Satakunta','Pirkanmaa','Etelä-Savo','Itä-Savo','Etelä-Karjala','Päijät-Häme','Kanta-Häme','Kymenlaakso','Varsinais-Suomi','Ahvenanmaa']
 
 module.exports = config
